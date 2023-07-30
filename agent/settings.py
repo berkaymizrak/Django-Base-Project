7a3c1b8f802793855952d53ec9e100d90686fcdf
Berkay MIZRAK <mizrakby@gmail.com>
2023-07-30 19:24:35 +0300

    feat: create urls

--- agent/settings.py
+++ agent/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'users.apps.UsersConfig',
     'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
 
     'django_celery_beat',
 ]
